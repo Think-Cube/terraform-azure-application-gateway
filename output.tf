664557b998f1e6c9ae582de2b980995f67c2b37d
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 12:40:11 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "id" {
   value       = azurerm_application_gateway.main.id
-  description = "The ID of the Application Gateway."
+  description = "The ID of the Application Gateway resource. This unique identifier is used to reference the Application Gateway within Azure."
   sensitive   = false
 }
