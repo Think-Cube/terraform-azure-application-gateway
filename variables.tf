852b0fe768c1171d8b49de48d2562da69b37f814
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 12:41:18 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,74 +1,66 @@
-###########################
-# Common vars
-###########################
 variable "environment" {
-  description = "Specifies the environment key used for naming backend containers. Defaults to 'dev'."
+  description = "The environment used for the backend container name key (e.g., dev, prod)."
   type        = string
   default     = "dev"
 }
 
 variable "default_tags" {
-  description = "A mapping of tags to assign to the resources for organizational and management purposes."
+  description = "A mapping of tags to assign to the resource for classification and management purposes."
   type        = map(any)
 }
 
 variable "region" {
-  description = "The geographical region where resources will be deployed. Defaults to 'weu' (West Europe)."
+  description = "The Azure region in which resources are deployed, typically used for geographic redundancy and availability."
   type        = string
   default     = "weu"
 }
 
-###########################
-# Resource groups vars
-###########################
 variable "resource_group_location" {
-  description = "The location/region where the resource group will be created. Changing this will force a new resource to be created. Defaults to 'West Europe'."
+  description = "The location/region where the Application Gateway is created. Changing this forces a new resource to be created."
   default     = "West Europe"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group in which resources will be created."
+  description = "The name of the resource group where the Application Gateway will be created."
   type        = string
 }
-############################
-# Application Gateway variables
-############################
+
 variable "subnet_name" {
   description = "The name of the subnet where the Application Gateway will be deployed."
   type        = string
 }
 
 variable "vnet_name" {
-  description = "The name of the Virtual Network that the Application Gateway will use."
+  description = "The name of the Virtual Network where the Application Gateway will be deployed."
   type        = string
 }
 
 variable "vnet_rg_name" {
-  description = "The name of the Resource Group where the Virtual Network exists."
+  description = "The name of the Resource Group containing the Virtual Network where the Application Gateway will be deployed."
   type        = string
 }
 
 variable "app_gateway_name" {
-  description = "The name assigned to the Application Gateway. Defaults to 'ApplicationGateway1'."
+  description = "The name of the Application Gateway. This will be used for reference and identification of the resource."
   type        = string
   default     = "ApplicationGateway1"
 }
 
 variable "app_gateway_sku" {
-  description = "The SKU name of the Application Gateway. This defines its pricing tier and capabilities. Defaults to 'Standard_v2'."
+  description = "The SKU (Stock Keeping Unit) of the Application Gateway. This determines the performance and capabilities of the Application Gateway."
   type        = string
   default     = "Standard_v2"
 }
 
 variable "app_gateway_tier" {
-  description = "The tier of the Application Gateway SKU, which determines its performance and features. Defaults to 'Standard_v2'."
+  description = "The tier of the Application Gateway SKU, affecting pricing and features. Defaults to 'Standard_v2'."
   type        = string
   default     = "Standard_v2"
 }
 
 variable "app_gateway_capacity" {
-  description = "The capacity (number of instances) of the Application Gateway. Defaults to 1."
+  description = "The capacity (number of instances) for the Application Gateway. This defines the scale and load capacity."
   type        = number
   default     = 1
 }
