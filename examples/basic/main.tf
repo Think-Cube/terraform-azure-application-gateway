5c2769b36902ede7df3646ce5b5f762960fdd274
Maciej Michalski <contact@mmichalski.com>
2026-09-21 18:06:21 +0200

    fix: apply azurerm v5 breaking changes to Application Gateway
    - enable_http2 renamed to http2_enabled
    - authentication_certificate block removed (use trusted_root_certificate)
    - Dynamic ssl_policy, waf_configuration, url_path_map, redirect_configuration
    - Upgrade to azurerm ~> 5.0, Terraform >= 1.9.0
    - Add .github/ community files, Azure Pipelines CI, LICENSE, .gitignore
    - Add examples/basic/ (minimal) and examples/advanced/ (full options), source = ../.. (relative path)
    - README generated by terraform-docs in both example directories

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 module "application_gateway" {
-  source = "github.com/Think-Cube/terraform-azure-application-gateway?ref=v1.0.0"
+  source = "../.."
 
   name                = "agw-dev-example"
   resource_group_name = "rg-example"
